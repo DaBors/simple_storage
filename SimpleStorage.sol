6c21c8726e97b0c164561fb53cf66b2e98261866
Márton Borsos <borsos.marton95@gmail.com>
2022-01-06 13:45:36 +0100

    First lesson complete

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.0 <0.9.0;
+pragma solidity ^0.6.0;
 
 contract SimpleStorage {
 
     uint256 favoriteNumber;
-
-    // This is a comment!
+    
     struct People {
         uint256 favoriteNumber;
         string name;
@@ -18,14 +17,14 @@ contract SimpleStorage {
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
     }
-    
-    function retrieve() public view returns (uint256){
+
+    function retrieve() public view returns(uint256){
         return favoriteNumber;
     }
-
+    
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         people.push(People(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;
     }
-}
 
+}
